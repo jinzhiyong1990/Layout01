6e75b202902619fd3636f2e01242005802892f95
jinzhiyong1990 <jinzhiyong1990@gmail.com@server.com>
2018-10-09 14:57:45 -0700

    finish linear layout

--- app/src/main/java/com/example/zhiyongjin/layout01/MainActivity.java
+++ app/src/main/java/com/example/zhiyongjin/layout01/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.cong2_5kaishi);
+        setContentView(R.layout.cong3_1kaishi);
     }
 }
